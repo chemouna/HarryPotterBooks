f5c44de5fa5babf1c2401eaf25864888acfdc41e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2015-12-03 09:31:39 +0100

    Setup book item view.

--- app/src/main/java/com/mounacheikhna/xebiaproject/ui/book/BookItemView.java
+++ app/src/main/java/com/mounacheikhna/xebiaproject/ui/book/BookItemView.java
@@ -3,10 +3,9 @@ package com.mounacheikhna.xebiaproject.ui.book;
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.mounacheikhna.xebiaproject.R;
@@ -18,19 +17,27 @@ import com.squareup.picasso.Picasso;
  */
 public class BookItemView extends RelativeLayout {//maybe extend CardView instead ?
 
-  @Bind(R.id.book_image) ImageView mBookImage;
+  @Bind(R.id.book_image) ImageView mImageView;
+  @Bind(R.id.book_name) TextView mNameView;
+  @Bind(R.id.book_author) TextView mAuthorView;
 
   public BookItemView(Context context) {
     super(context);
   }
 
+  public BookItemView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
   public void bindTo(Book book, Picasso picasso) {
+    mNameView.setText(book.getTitle());
+    //mAuthorView.setText(book.get);
     if (!TextUtils.isEmpty(book.getCover())) {
       picasso.load(book.getCover())
           //.placeholder(R.drawable.ic_city)
           //.error(R.drawable.ic_city)
           .fit()
-          .into(mBookImage);
+          .into(mImageView);
     }
   }
 
