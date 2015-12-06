52b7d8294853c20c91c88e63aba3f79a86270ca1
Mouna Cheikhna <cheikhnamouna@gmail.com>
2015-12-06 12:07:32 +0100

    Add rating and other book details and display them.

--- app/src/main/java/com/mounacheikhna/xebiaproject/api/model/BookResponse.java
+++ app/src/main/java/com/mounacheikhna/xebiaproject/api/model/BookResponse.java
@@ -1,38 +1,52 @@
 package com.mounacheikhna.xebiaproject.api.model;
 
+import android.support.annotation.Nullable;
 import java.util.ArrayList;
 import org.simpleframework.xml.Element;
-import org.simpleframework.xml.ElementArray;
 import org.simpleframework.xml.ElementList;
-import org.simpleframework.xml.Path;
 import org.simpleframework.xml.Root;
 
 /**
  * Created by mouna on 06/12/15.
  */
-@Root(name = "GoodreadsResponse", strict = false)
-public class BookResponse {
+@Root(name = "GoodreadsResponse", strict = false) public class BookResponse {
 
-  @Element(name = "Request")
-  Request request;
+  @Element(name = "Request") Request request;
 
-  @Element(name = "search", required = false)
-  Search search;
+  @Element(name = "search", required = false) Search search;
 
   public Request getRequest() {
     return request;
   }
 
-  @Root(strict = false)
-  public static class Request {
-    @Element(name = "key")
-    String key;
+  public Search getSearch() {
+    return search;
+  }
 
-    @Element(name = "method")
-    String method;
+  @Nullable
+  public GoodreadsBook getFirstBook() {
+    if (search == null || search.results == null || search.results.works == null ||
+        search.results.works.size() == 0) {
+      return null;
+    }
+    return search.results.works.get(0).mGoodreadsBook;
+  }
 
-    @Element(name = "authentication")
-    String authentication;
+  @Nullable
+  public Work getFirsWork() {
+    if (search == null || search.results == null || search.results.works == null ||
+        search.results.works.size() == 0) {
+      return null;
+    }
+    return search.results.works.get(0);
+  }
+
+  @Root(strict = false) public static class Request {
+    @Element(name = "key") String key;
+
+    @Element(name = "method") String method;
+
+    @Element(name = "authentication") String authentication;
 
     public String getKey() {
       return key;
@@ -47,51 +61,58 @@ public class BookResponse {
     }
   }
 
-  @Root(strict = false)
-  public static class Search {
-    @Element(name = "results", required = false)
-    Results results;
+  @Root(strict = false) public static class Search {
+    @Element(name = "results", required = false) Results results;
   }
 
-  @Root(strict = false)
-  public static class Results {
-    @ElementList(name="work", required = false, inline = true)
-    ArrayList<Work> works;
+  @Root(strict = false) public static class Results {
+    @ElementList(name = "work", required = false, inline = true) ArrayList<Work> works;
   }
 
-  @Root(strict = false)
-  public static class Work {
+  @Root(strict = false) public static class Work {
 
-    @Element(name="best_book", required = false)
-    Book book;
+    @Element(name = "best_book", required = false) GoodreadsBook mGoodreadsBook;
+
+    @Element(name = "ratings_count", required = false) int ratingsCount;
+    @Element(name = "text_reviews_count", required = false) int textReviewsCount;
+    @Element(name = "average_rating", required = false) String averageRating;
+
+    @Element(name = "original_publication_year", required = false) int originalPublicationYear;
+    @Element(name = "original_publication_month", required = false) int originalPublicationMonth;
+    @Element(name = "original_publication_day", required = false) int originalPublicationDay;
+
+    public String getAverageRating() {
+      return averageRating;
+    }
+
+    public int getRatingsCount() {
+      return ratingsCount;
+    }
+
+    public int getOriginalPublicationYear() {
+      return originalPublicationYear;
+    }
+
+    public int getOriginalPublicationMonth() {
+      return originalPublicationMonth;
+    }
+
+    public int getOriginalPublicationDay() {
+      return originalPublicationDay;
+    }
+
+
   }
 
-  @Root(strict = false)
-  public static class Book {
+  @Root(strict = false) public static class GoodreadsBook {
 
-    @Element(name = "id", required = false)
-    String id;
-
-    @Element(name = "title", required = false)
-    String title;
-
-    @Element(name = "isbn", required = false)
-    String isbn;
-
-    @Element(name = "image_url", required = false)
-    String imageUrl;
-
-    @Element(name = "small_image_url", required = false)
-    String smallImageUrl;
-
-    @Element(name = "description", required = false)
-    String description;
-
-    @Element(name = "average_rating", required = false)
-    String averageRating;
-
-    @Element(name = "authors", required = false)
-    Authors authors;
+    @Element(name = "id", required = false) String id;
+    @Element(name = "title", required = false) String title;
+    @Element(name = "isbn", required = false) String isbn;
+    @Element(name = "image_url", required = false) String imageUrl;
+    @Element(name = "small_image_url", required = false) String smallImageUrl;
+    @Element(name = "description", required = false) String description;
+    @Element(name = "author", required = false) Author author;
 
     public String getId() {
       return id;
@@ -101,51 +122,21 @@ public class BookResponse {
       return title;
     }
 
-    public String getIsbn() {
-      return isbn;
-    }
-
-    public String getImageUrl() {
-      return imageUrl;
-    }
-
-    public String getSmallImageUrl() {
-      return smallImageUrl;
-    }
-
     public String getDescription() {
       return description;
     }
-
-    public String getAverageRating() {
-      return averageRating;
-    }
-
-    public Authors getAuthors() {
-      return authors;
-    }
-  }
-
-  @Root(strict = false)
-  public static class Authors {
-
-    @Element(name = "author", required = false)
-    Author author;
 
     public Author getAuthor() {
       return author;
     }
   }
 
-  @Root(strict = false)
-  public static class Author {
+  @Root(strict = false) public static class Author {
 
-    @Element(name = "name", required = false)
-    String name;
+    @Element(name = "name", required = false) String name;
 
     public String getName() {
       return name;
     }
   }
-
 }
--- app/src/main/java/com/mounacheikhna/xebiaproject/ui/details/BookDetailsView.java
+++ app/src/main/java/com/mounacheikhna/xebiaproject/ui/details/BookDetailsView.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.mounacheikhna.xebiaproject.HenriPotierApp;
 import com.mounacheikhna.xebiaproject.R;
 import com.mounacheikhna.xebiaproject.api.model.Book;
 import com.mounacheikhna.xebiaproject.api.model.BookResponse;
+import com.mounacheikhna.xebiaproject.ui.view.ExpandingTextView;
 import javax.inject.Inject;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 import timber.log.Timber;
 
 /**
@@ -19,6 +23,11 @@ import timber.log.Timber;
 public class BookDetailsView extends LinearLayout implements BookDetailsScreen {
 
   @Inject BookDetailsPresenter mBookDetailsPresenter;
+
+  @Bind(R.id.description) ExpandingTextView mDescriptionView;
+  @Bind(R.id.author) TextView mAuthorView;
+  @Bind(R.id.tv_rating_value) TextView mRatingValueView;
+  @Bind(R.id.tv_rating_votes) TextView mRatingVotesView;
 
   public BookDetailsView(Context context) {
     super(context);
@@ -45,6 +54,7 @@ public class BookDetailsView extends LinearLayout implements BookDetailsScreen {
 
   public void display(Book book) {
     mBookDetailsPresenter.searchGoodreadsBook(book.getTitle())
+        .observeOn(AndroidSchedulers.mainThread())
         .subscribe(new Subscriber<BookResponse>() {
           @Override public void onCompleted() {
           }
@@ -55,7 +65,35 @@ public class BookDetailsView extends LinearLayout implements BookDetailsScreen {
 
           @Override public void onNext(BookResponse bookResponse) {
             Timber.d("bookResponse : %s", bookResponse);
+            displayGoodreadsDetails(bookResponse);
           }
         });
   }
+
+  private void displayGoodreadsDetails(BookResponse bookResponse) {
+    if(bookResponse == null) return;
+    final BookResponse.GoodreadsBook goodreadsBook = bookResponse.getFirstBook();
+    if(goodreadsBook != null) {
+      mDescriptionView.setText(goodreadsBook.getDescription());
+      final BookResponse.Author author = goodreadsBook.getAuthor();
+      if(author != null) {
+        mAuthorView.setText(author.getName());
+      }
+    }
+
+    final BookResponse.Work work = bookResponse.getFirsWork();
+    if(work != null) {
+      mRatingValueView.setText(work.getAverageRating());
+      mRatingVotesView.setText(buildRatingVotesValue(work.getRatingsCount()));
+    }
+
+  }
+
+  public String buildRatingVotesValue(Integer votes) {
+    if (votes == null || votes < 0) {
+      votes = 0;
+    }
+    return getResources().getQuantityString(R.plurals.votes, votes, votes);
+  }
+
 }
